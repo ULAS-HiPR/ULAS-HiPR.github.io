293d8c652069a4429f4ecc466962da50c110ef4c
Derv6464 <dervlag6464@gmail.com>
2023-10-20 18:17:38 +0100

    inital react commit

---
+++ BasicSite/README.md
@@ -0,0 +1,3 @@
+# ULAS-basic-site
+
+A quick and simple website for ULAS HiPER Rocket team. This is a temporary website until the actual one has been made
---
+++ src/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './css/NavBar.css';
+
+const NavBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/">About</a></li>
+        <li><a href="/">Services</a></li>
+        <li><a href="/">Contact</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
